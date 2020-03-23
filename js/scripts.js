2a80613f1e5395283d84079677b6b6f3b616a81f
Christopher Huber <CHuber42.work@gmail.com>
2020-03-23 14:18:22 -0700

    downloaded instance from Chi

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,11 @@
-jQuery("h1").click(function(){
-  alert("This is a header.");
-  alert("I told you, THIS IS A HEADER!");
+$(document).ready(function(){
+    
+  var userInput = prompt("Please enter a sentence.");
+//   console.log(userInput);
+  alert(userInput);
 });
 
-jQuery("p").click(function(){
-  alert("This is a paragraph.");
-});
 
-jQuery("img").click(function(){
-  alert("This is an image.");
-});
 
+var end = userInput.slice(-1)
+var beg = userInput.slice(0)
