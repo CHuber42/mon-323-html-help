88caaf526bdf884b9050d7c1496901b5ca4d35fc
Christopher Huber <CHuber42.work@gmail.com>
2020-03-23 15:02:39 -0700

    playing around with a lot of js stuff

--- js/scripts.js
+++ js/scripts.js
@@ -3,9 +3,27 @@ $(document).ready(function(){
   var userInput = prompt("Please enter a sentence.");
 //   console.log(userInput);
   alert(userInput);
+  var end2 = userInput.charAt(0).toUpperCase() + userInput.charAt(userInput.length-1).toUpperCase();
+  var inverted = end2.charAt(1) + end2.charAt(0);
+
+
+  alert(end2);
+  alert(inverted);
+});
+ 
+
+$(document).ready(function(){
+  $('div').click(function(){
+    alert('hello');
+  });
 });
 
+//  var end2 = userInput.charAt(0).toUpperCase() + userInput.charAt(4).toUpperCase();
+//  alert(end2);
 
 
-var end = userInput.slice(-1)
-var beg = userInput.slice(0)
+
+// apple
+// ApplE
+
+// https://joshtronic.com/2016/02/14/how-to-capitalize-the-first-letter-in-a-string-in-javascript/
